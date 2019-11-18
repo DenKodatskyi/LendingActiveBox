c8cb08b65529ea1306826b123474a35c03b9d90f
DenKodatskyi <DENKODAK@gmail.com>
2019-11-18 21:46:25 +0200

    00_LendingActiveBox

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+/* Header */
+let header = document.querySelector(".js_header");
+let intro = document.querySelector(".js_intro");
+
+header.classList.add("fixed");
+
+console.log(header);
+
+console.log(onclick);
+
+document.onwheel = (e)=> {
+   console.log(e);   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
